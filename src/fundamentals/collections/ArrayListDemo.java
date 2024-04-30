50956c9ec4e3b5b955ca5952963414e367d4d51c
sreehari0101 <sreehariprolos@gmail.com>
2024-04-30 23:43:34 +0530

    ArrayList

---
+++ src/fundamentals/collections/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package fundamentals.collections;
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args){
+        List<Integer> numbers = List.of(1,4,2,6,7);
+        ArrayList<Integer> arr = new ArrayList<Integer>(numbers);
+        System.out.println(arr);
+
+        arr.add(5);
+        System.out.println(arr);
+        arr.add(2,8);
+        System.out.println(arr);
+        arr.remove(2);
+        System.out.println(arr);
+
+        
+
+    }
+}
